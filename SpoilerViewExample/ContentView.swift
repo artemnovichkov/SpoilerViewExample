c215ef13751020bec3fee52426d14a23bb233ec3
Artem Novichkov <mail@artemnovichkov.com>
2023-02-27 18:09:28 +0600

    Initial Commit

---
+++ SpoilerViewExample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SpoilerViewExample
+//
+//  Created by Artem Novichkov on 27.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpoilerViewExample/SpoilerViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpoilerViewExampleApp.swift
+//  SpoilerViewExample
+//
+//  Created by Artem Novichkov on 27.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpoilerViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
